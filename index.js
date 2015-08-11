d4a6d72c62bab9b7b67911d618036500c442c1f3
helios_m <revan.den@gmail.com>
2015-08-11 12:47:22 +0300

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function () {
+  var babel = require('babel-core');
+  var fs = require('fs');
+
+  require('babel-core/polyfill');
+
+  function compile(filename) {
+    console.log(filename);
+    var result = babel.transformFileSync(filename, { optional: ['bluebirdCoroutines'] });
+    result = babel.transform(result.code);
+    return result.code;
+  }
+
+  require.extensions['.js'] = function (m, filename) {
+    if (filename.indexOf('node_modules') > -1) {
+      m._compile('' + fs.readFileSync(filename), filename);
+    }
+    else {
+      m._compile(compile(filename), filename);
+    }
+  };
+};
